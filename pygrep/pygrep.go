f8f169d798f06e21deb7c502a6a3326c6b6e0433
Eugenio Dols <eudols@gmail.com>
2016-06-20 16:14:57 -0700

    Rename file to pygrep to reflect it will grep on Python files by default

--- pygrep/pygrep.go
+++ pygrep/pygrep.go
@@ -8,30 +8,31 @@ import (
 
 func main() {
 	pattern := ""
-	filepattern := `\.(h|c|cc|cpp)$` // default to source files
-	var argM, argC, argP bool
+	filepattern := `\.py$` // default to Python source files
+	var argC, argM, argJ bool
 
 	flag.Usage = func() {
-		fmt.Println("Usage: cgrep [-mp] [-f <file regexp>] <regexp>")
+		fmt.Println("Usage: pygrep [-mcj] [-f <file regexp>] <regexp>")
 		fmt.Println("  -h: this help")
+		fmt.Println("  -c: search in C files")
 		fmt.Println("  -m: search in make files")
-		fmt.Println("  -p: search in python files")
+		fmt.Println("  -j: search in javascript files")
 		fmt.Println("  -f: use an alternate file regular expression")
 		fmt.Println("  (see \"man perlre\" for perl regular expressions)")
 	}
 
 	flag.BoolVar(&argC, "c", false, "")
 	flag.BoolVar(&argM, "m", false, "")
-	flag.BoolVar(&argP, "p", false, "")
+	flag.BoolVar(&argJ, "j", false, "")
 	flag.StringVar(&filepattern, "f", `\.(h|c|cc|cpp)$`, "")
 	flag.Parse()
 
 	if argC {
-		filepattern = `^Cons(cript|struct)$`
+		filepattern = `\.(h|c|cc|cpp)$`
 	} else if argM {
 		filepattern = `([Mm]ake[^~]*|\.mk)$`
-	} else if argP {
-		filepattern = `\.py$`
+	} else if argJ {
+		filepattern = `\.js$`
 	}
 	// if no arguments, print usage and exit
 	if flag.NArg() != 1 {
