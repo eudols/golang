ed044433357bbee89aaeb65782dfa126fd7e3d6f
Eugenio Dols <eudols@gmail.com>
2016-12-08 20:12:30 -0800

    Concurrent version
    First cut of concurrent version. So far it seems to work fine and
    produce the exact same output as the serialized version

--- pygrep/pygrep.go
+++ pygrep/pygrep.go
@@ -8,9 +8,26 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strconv"
+	"sync"
 )
 
-func fileSearch(fullpath string, pattern string) {
+var wg sync.WaitGroup
+
+// Takes a channel that receives string channels. Each matching file
+// will have its own string channel. This function will iterate over
+// each one in order
+func collectLines(pipe chan chan string) {
+	defer wg.Done()
+	for a := range pipe {
+		for str := range a {
+			fmt.Println(str)
+		}
+	}
+}
+
+func fileSearch(fullpath string, pattern string, lines chan string) {
+	defer wg.Done()
 	lineNumber := 0
 	// fmt.Printf("I am in fileSearch for %s , and file pattern %s\n", fullpath, pattern)
 	var regExp = regexp.MustCompile(pattern)
@@ -18,17 +35,20 @@ func fileSearch(fullpath string, pattern string) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "*** Could not open %s\n", fullpath)
 	}
+
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		lineNumber++
 		if regExp.MatchString(input.Text()) {
-			fmt.Printf("%s:%d %s\n", fullpath, lineNumber, input.Text())
+			//fmt.Printf("%s:%d %s\n", fullpath, lineNumber, input.Text())
+			lines <- (fullpath + ":" + strconv.Itoa(lineNumber) + " " + input.Text())
 		}
 	}
+	close(lines)
 	f.Close()
 }
 
-func printFile(filepattern string, pattern string) filepath.WalkFunc {
+func printFile(filepattern string, pattern string, a chan chan string) filepath.WalkFunc {
 	return func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			log.Print(err)
@@ -41,7 +61,10 @@ func printFile(filepattern string, pattern string) filepath.WalkFunc {
 
 		if regExp.MatchString(info.Name()) {
 			// fmt.Printf("File %s matches\n", info.Name())
-			fileSearch(path, pattern)
+			wg.Add(1)
+			lines := make(chan string, 100)
+			a <- lines
+			go fileSearch(path, pattern, lines)
 		}
 		return nil
 	}
@@ -100,12 +123,21 @@ func main() {
 	// fmt.Println("filepattern ", filepattern)
 	// fmt.Println("pattern ", pattern)
 
+	wg.Add(1)
+	a := make(chan chan string)
+	go collectLines(a)
 	log.SetFlags(log.Lshortfile)
 	dir := "."
-	err := filepath.Walk(dir, printFile(filepattern, pattern))
+	//dir := "/neteng/edols/supportassist/AR-9289-ETL-replacement/ngos/serv-supportassist/lib"
+	err := filepath.Walk(dir, printFile(filepattern, pattern, a))
 	if err != nil {
 		log.Fatal(err)
 	}
+	wg.Done()
+	wg.Wait()
+	wg.Add(1)
+	close(a)
+	wg.Wait()
 	// argsWithProg := os.Args
 	// argsWithoutProg := os.Args[1:]
 	//
