949d6b4eb7e949da42d8f3a38785e86a21713efd
Eugenio Dols <eudols@gmail.com>
2016-12-09 17:14:52 -0800

    Added verbose flag and done cleanup
    Cleanup of commented out code.
    Added -v verbose flag to print additional information that could
    help on debugging issues.

--- pygrep/pygrep.go
+++ pygrep/pygrep.go
@@ -1,3 +1,5 @@
+// Searches recursively for provided regular expression in Python files
+// by default
 package main
 
 import (
@@ -12,6 +14,7 @@ import (
 	"sync"
 )
 
+var verbose bool
 var wg sync.WaitGroup
 
 // Takes a channel that receives string channels. Each matching file
@@ -29,7 +32,9 @@ func collectLines(pipe chan chan string) {
 func fileSearch(fullpath string, pattern string, lines chan string) {
 	defer wg.Done()
 	lineNumber := 0
-	// fmt.Printf("I am in fileSearch for %s , and file pattern %s\n", fullpath, pattern)
+	if verbose {
+		fmt.Printf("In fileSearch for %s , and search pattern %s\n", fullpath, pattern)
+	}
 	var regExp = regexp.MustCompile(pattern)
 	f, err := os.Open(fullpath)
 	if err != nil {
@@ -40,7 +45,9 @@ func fileSearch(fullpath string, pattern string, lines chan string) {
 	for input.Scan() {
 		lineNumber++
 		if regExp.MatchString(input.Text()) {
-			//fmt.Printf("%s:%d %s\n", fullpath, lineNumber, input.Text())
+			if verbose {
+				fmt.Printf("Matched: %s:%d %s\n", fullpath, lineNumber, input.Text())
+			}
 			lines <- (fullpath + ":" + strconv.Itoa(lineNumber) + " " + input.Text())
 		}
 	}
@@ -54,13 +61,17 @@ func printFile(filepattern string, pattern string, a chan chan string) filepath.
 			log.Print(err)
 			return nil
 		}
-		// fmt.Println("printFile filepattern ", filepattern)
-		// fmt.Println("printFile pattern ", pattern)
-		// fmt.Println("printFile path ", path)
+		if verbose {
+			fmt.Println("printFile filepattern ", filepattern)
+			fmt.Println("printFile search pattern ", pattern)
+			fmt.Println("printFile path ", path)
+		}
 		var regExp = regexp.MustCompile(filepattern)
 
 		if regExp.MatchString(info.Name()) {
-			// fmt.Printf("File %s matches\n", info.Name())
+			if verbose {
+				fmt.Printf("File %s matches filepattern\n", info.Name())
+			}
 			wg.Add(1)
 			lines := make(chan string, 100)
 			a <- lines
@@ -70,25 +81,10 @@ func printFile(filepattern string, pattern string, a chan chan string) filepath.
 	}
 }
 
-//
-// func printFile(path string, info os.FileInfo, err error) error {
-// 	if err != nil {
-// 		log.Print(err)
-// 		return nil
-// 	}
-// 	if info.IsDir() {
-// 		fmt.Println("I am a directory, skip me")
-// 		return nil
-// 	}
-// 	fmt.Print(info.Name())
-// 	fmt.Println(path)
-// 	return nil
-// }
-
 func main() {
 	pattern := ""
 	filepattern := `\.py$` // default to Python source files
-	var argC, argM, argJ bool
+	var argC, argM, argJ, argV bool
 
 	flag.Usage = func() {
 		fmt.Println("Usage: pygrep [-mcj] [-f <file regexp>] <regexp>")
@@ -97,12 +93,14 @@ func main() {
 		fmt.Println("  -m: search in make files")
 		fmt.Println("  -j: search in javascript files")
 		fmt.Println("  -f: use an alternate file regular expression")
+		fmt.Println("  -v: verbose output")
 		fmt.Println("  (see \"https://golang.org/pkg/regexp/\" for details on regular expressions)")
 	}
 
 	flag.BoolVar(&argC, "c", false, "")
 	flag.BoolVar(&argM, "m", false, "")
 	flag.BoolVar(&argJ, "j", false, "")
+	flag.BoolVar(&argV, "v", false, "")
 	flag.StringVar(&filepattern, "f", `\.py$`, "")
 	flag.Parse()
 
@@ -113,6 +111,9 @@ func main() {
 	} else if argJ {
 		filepattern = `\.js$`
 	}
+	if argV {
+		verbose = true
+	}
 	// if no arguments, print usage and exit
 	if flag.NArg() != 1 {
 		flag.Usage()
@@ -120,15 +121,16 @@ func main() {
 	}
 
 	pattern = flag.Arg(0)
-	// fmt.Println("filepattern ", filepattern)
-	// fmt.Println("pattern ", pattern)
+	if verbose {
+		fmt.Println("filepattern ", filepattern)
+		fmt.Println("pattern ", pattern)
+	}
 
 	wg.Add(1)
 	a := make(chan chan string)
 	go collectLines(a)
 	log.SetFlags(log.Lshortfile)
 	dir := "."
-	//dir := "/neteng/edols/supportassist/AR-9289-ETL-replacement/ngos/serv-supportassist/lib"
 	err := filepath.Walk(dir, printFile(filepattern, pattern, a))
 	if err != nil {
 		log.Fatal(err)
@@ -138,9 +140,4 @@ func main() {
 	wg.Add(1)
 	close(a)
 	wg.Wait()
-	// argsWithProg := os.Args
-	// argsWithoutProg := os.Args[1:]
-	//
-	// fmt.Println(argsWithProg)
-	// fmt.Println(argsWithoutProg)
 }
